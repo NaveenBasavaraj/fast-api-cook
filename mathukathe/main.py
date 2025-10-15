869e0bdcb09c31d1fe7fc8a8c6e1b217c5f4450e
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2025-10-15 19:40:11 +0530

    added app methods to create and get posts with FastAPI() instance

--- mathukathe/main.py
+++ mathukathe/main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+# model is used to validate data
+# using base model
 
 
-@app.get("/")
-async def root() -> str:
-    """
-    greetings
-    """
-    return {"message": "Hello World!"}
+class UserPostIn(BaseModel):
+    """ """
+
+    body: str
 
 
-@app.get("/post")
-async def add_post() -> str:
-    return
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.get("/get_post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+
+
+@app.post("/add_post", response_model=UserPostIn)
+async def create_post(mathukathe_post: UserPostIn):
+    # post.body -> json payload
+    data = mathukathe_post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
