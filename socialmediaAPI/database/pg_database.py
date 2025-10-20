462eba6e3d79b0f6759d03102edb5ce722888fef
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2025-10-21 00:19:05 +0530

    increase attempts made to login to db

--- socialmediaAPI/database/pg_database.py
+++ socialmediaAPI/database/pg_database.py
@@ -8,16 +8,19 @@ database = os.getenv("DATABASE")
 user = os.getenv("USER")
 password = os.getenv("PASSWORD")
 
-
-try:
-    conn = psycopg2.connect(
-        host=host,
-        database=database,
-        user=user,
-        password=password,
-        cursor_factory=RealDictCursor,
-    )
-    cursor = conn.cursor()
-    print("Database Connection was successful")
-except Exception as e:
-    print(f"error connecting to db {e}")
+attempts = 1
+while attempts <= 2:
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database Connection was successful")
+        break
+    except Exception as e:
+        attempts += 1
+        print(f"error connecting to db {e}")
